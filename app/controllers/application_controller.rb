683ebbfed665340349dafb51deb1016b83b47d69
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-09-17 01:55:04 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parmitted_parameter, if: :devise_controller?
+
+  protected
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:username])
+  end
 end
