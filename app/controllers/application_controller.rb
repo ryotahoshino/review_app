c668deaf1f5a254776acc9782abad786770b973c
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-09-18 00:38:24 +0900

    コントローラーとSCSSの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_parmitted_parameter, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_parmitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, key: [:username])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
