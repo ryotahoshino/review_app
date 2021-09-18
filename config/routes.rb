adbcca445f2471ac8ba56b361cb0d5c040ace421
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-09-18 13:23:25 +0900

    ルートの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'subjects/index'
-  get 'subjects/show'
   devise_for :users
   root 'homes#top'
+  resources :subjects, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
