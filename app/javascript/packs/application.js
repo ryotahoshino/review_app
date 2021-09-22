a513b4253b9fa1467f5520916d3e8e295f4307c5
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-09-22 16:53:14 +0900

    jsの編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require_tree .
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("turbolinks").start()
+//require("./reviews")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
