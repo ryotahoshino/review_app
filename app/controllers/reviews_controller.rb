4aa8d3bfbf3363bc536e14505486bd10108f30fb
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-10-03 14:56:45 +0900

    コントローラーなどの編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to subject_reviews_path(@review.subject)
+    else
+      @subject = Subject.find(params[:id])
+      render "subjects/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:subject_id, :score, :content)
+  end
 end
